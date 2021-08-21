5e326f856d672fa76ccbe453559a0ef35db81272
parthshingari28 <parthshingari14@gmail.com>
2021-08-21 11:43:48 +0530

    Added efficient approach to solve trailing zeroes problem

--- maths/trailing_zeroes_in_factorial.cpp
+++ maths/trailing_zeroes_in_factorial.cpp
@@ -1,5 +1,5 @@
 // Trailing zeroes in a factorial of a number -> count of zeros at end of factorial value of number
-
+// Naive Approach
 #include<iostream>
 #include<limits.h>
 using namespace std;
@@ -17,8 +17,24 @@ int countZero(int n)
     return res;
 }
 
+// Time Complexicity = 0(n)
+// Major issue with the solution is it going to cause overflow for even slightly higher values of n and it would be difficult for int to hold these values.
+
+// Efficient Approach
+int countTrailingZeroes(int n)
+{
+    int res = 0;
+    for(int i = 5; i <= n; i = i*5)
+        res = res + n/i;
+    return res;
+}
+
+// Time complexicity : 0(log n)
+// It also solves the major issue of overflow too, as we are not computing value of factorial in this approach.
+
 int main(){
-    int number = 10;
-    cout<<countZero(number);
+    int number = 351;
+   // cout<<countZero(number);
+    cout<<countTrailingZeroes(number);
     return 0;
 }
