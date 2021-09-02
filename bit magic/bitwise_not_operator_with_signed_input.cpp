cf2e6d5b19300f160a6276fc1aaa0c2131389703
parthshingari28 <parthshingari14@gmail.com>
2021-09-02 13:28:24 +0530

    Adding bitwise not operator with signed input value

---
+++ bit magic/bitwise_not_operator_with_signed_input.cpp
@@ -0,0 +1,14 @@
+// Bitwise not operator -> Signed Input
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 1;
+	cout<<(~ x)<<endl;
+	x = 5;
+	cout<<(~ x)<<endl;
+	return 0;
+}
+
+// Genarlly, it is not recommended to use bitwise operators with negative numbers.
